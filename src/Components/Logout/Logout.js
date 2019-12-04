5b26a3ec3fc21456d3406e64ad2bb86f8d408c8e
Sam Wheeling <anchormansam@gmail.com>
2019-12-04 14:40:43 -0500

    trying to find firebase issues

--- src/Components/Logout/Logout.js
+++ src/Components/Logout/Logout.js
@@ -16,40 +16,35 @@ export default class Logout extends React.Component {
 
     this.handleClick = this.handleClick.bind(this);
   }
+  componentDidMount() {
+    console.log("Mounted logout");
+  }
 
   handleClick(event) {
+    console.log("in handle click");
     axios({
       url: "/logout",
       method: "get",
       baseURL: "https://bangachain.appspot.com/api",
 
       headers: { Authorization: "Bearer " + this.props.token }
-    }).then(
-      res=>{
-        // console.log("caught response:",res)
-      }
-    ).catch(
-      res=>{
-        // console.log("caught error:",res)
-      }
-    );
-    localStorage.clear();
-    this.props.setToken("");
+    })
+      .then(res => {
+        console.log("caught response:", res);
+        localStorage.clear();
+        this.props.setToken("");
+      })
+      .catch(res => {
+        console.log("caught error:", res);
+      });
     event.preventDefault();
   }
 
   render() {
     return (
-  <NavbarBrand 
-    href="/" 
-    to="/" 
-    id="logoutTitle"
-    onClick={this.handleClick}
-  >
-      Logout
-                    
-  </NavbarBrand>
-    )
- 
+      <NavbarBrand href="/" to="/" id="logoutTitle" onClick={this.handleClick}>
+        Logout
+      </NavbarBrand>
+    );
   }
 }
